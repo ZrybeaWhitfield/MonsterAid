a302c65859aec1a9d612a704fe0705eb84714b11
ZrybeaWhitfield <zeathebeast@gmail.com>
2024-07-23 10:35:45 -0700

    Integrating Clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [clerk()],
+  adapter: node({ mode: "standalone" }),
+  output: "server",
+});
